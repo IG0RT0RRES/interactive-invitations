f08c7044115a3df691f71cf17679c6df9673dc7a
Igor Torres <igortorres234@gmail.com>
2026-09-10 20:16:37 -0300

    Ajuste de importação desnecessárias !

--- src/templates/aniversario/TemplateToyStory.jsx
+++ src/templates/aniversario/TemplateToyStory.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { supabase } from '../../services/supabaseClient';
 
 export default function TemplateToyStory({ slug, eventoData }) {
